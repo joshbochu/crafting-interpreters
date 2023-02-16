4547fdd9fb2e33dd8d1a8c32c10ec45ab4bc9456
joshbochu <joshuatbochu+git@gmail.com>
2023-02-16 01:01:06 -0800

    update stuff

--- src/scanner.ts
+++ src/scanner.ts
@@ -1,14 +1,35 @@
-import { Token } from './token';
+import { Token, TokenType } from './token';
 
 class Scanner {
     source: string;
+    tokens: Token[] = [];
+    start = 0;
+    current = 0;
+    line = 1;
 
     constructor(source: string) {
         this.source = source;
     }
 
     public scanTokens(): Token[] {
-        return [];
+        while (!this.isAtEnd()) {
+            this.start = this.current;
+            this.scanToken();
+        }
+        this.tokens.push(new Token(TokenType.EOF, '', null, this.line));
+        return this.tokens;
+    }
+
+    public scanToken() {
+        const c = '';
+        // const c = advance();
+        switch (c) {
+            case '('
+        }
+    }
+
+    public isAtEnd(): boolean {
+        return this.current >= this.source.length;
     }
 }
 export default Scanner;
