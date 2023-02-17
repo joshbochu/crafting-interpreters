0605184345a2c22398ffa5b7a9fa728a33c3c94e
joshbochu <joshuatbochu+git@gmail.com>
2023-02-16 21:06:55 -0800

    add create token

--- src/scanner.ts
+++ src/scanner.ts
@@ -2,7 +2,7 @@ import { Token, TokenType } from './token';
 
 class Scanner {
     source: string;
-    tokens: Token[] = [];
+    tokens: Token[];
     start = 0;
     current = 0;
     line = 1;
@@ -20,12 +20,57 @@ class Scanner {
         return this.tokens;
     }
 
-    public scanToken() {
-        const c = '';
-        // const c = advance();
+    private scanToken() {
+        const c = this.advance();
         switch (c) {
-            case '('
+            case '(':
+                this.addToken(TokenType.LEFT_PAREN);
+                break;
+            case ')':
+                this.addToken(TokenType.RIGHT_PAREN);
+                break;
+            case '{':
+                this.addToken(TokenType.LEFT_BRACE);
+                break;
+            case '}':
+                this.addToken(TokenType.RIGHT_BRACE);
+                break;
+            case ',':
+                this.addToken(TokenType.COMMA);
+                break;
+            case '.':
+                this.addToken(TokenType.DOT);
+                break;
+            case '-':
+                this.addToken(TokenType.MINUS);
+                break;
+            case '+':
+                this.addToken(TokenType.PLUS);
+                break;
+            case ';':
+                this.addToken(TokenType.SEMICOLON);
+                break;
+            case '*':
+                this.addToken(TokenType.STAR);
+                break;
+            default:
+                break;
         }
+    }
+
+    public addToken(type: TokenType) {
+        this.addTokenWithLiteral(type, null);
+        return;
+    }
+
+    public addTokenWithLiteral(type: TokenType, literal: unknown) {
+        const text = this.source.substring(this.start, this.current);
+        const token = new Token(type, text, literal, this.line);
+        this.tokens.push(token);
+    }
+
+    public advance(): string {
+        return this.source.charAt(++this.current);
     }
 
     public isAtEnd(): boolean {
