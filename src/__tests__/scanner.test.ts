1895fc513cde5ad73f9885bf30df23ff12f6de2e
joshbochu <joshuatbochu+git@gmail.com>
2023-02-18 13:56:57 -0800

    add tests

--- src/__tests__/scanner.test.ts
+++ src/__tests__/scanner.test.ts
@@ -1,7 +1,15 @@
+import { Token, TokenType } from '../token';
 import { Scanner } from '../scanner';
 
 test('produces correct tokens', () => {
     const scanner = new Scanner(`var foo = "bar"`);
     const actual = scanner.scanTokens();
-    expect(actual).toBe([]);
+    const expected = [
+        new Token(TokenType.VAR, 'var', null, 1),
+        new Token(TokenType.IDENTIFIER, 'foo', null, 1),
+        new Token(TokenType.EQUAL, '=', null, 1),
+        new Token(TokenType.STRING, `"bar"`, 'bar', 1),
+        new Token(TokenType.EOF, '', null, 1)
+    ];
+    expect(actual).toStrictEqual(expected);
 });
--- src/token.ts
+++ src/token.ts
@@ -1,69 +1,4 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-class Token {
-    type: TokenType;
-    lexeme: string;
-    literal: any;
-    line: number;
-
-    TOKEN_TYPE_NAMES = {
-        [TokenType.LEFT_PAREN]: 'LEFT_PAREN',
-        [TokenType.RIGHT_PAREN]: 'RIGHT_PAREN',
-        [TokenType.LEFT_BRACE]: 'LEFT_BRACE',
-        [TokenType.RIGHT_BRACE]: 'RIGHT_BRACE',
-        [TokenType.COMMA]: 'COMMA',
-        [TokenType.DOT]: 'DOT',
-        [TokenType.MINUS]: 'MINUS',
-        [TokenType.PLUS]: 'PLUS',
-        [TokenType.SEMICOLON]: 'SEMICOLON',
-        [TokenType.SLASH]: 'SLASH',
-        [TokenType.STAR]: 'STAR',
-        [TokenType.BANG]: 'BANG',
-        [TokenType.BANG_EQUAL]: 'BANG_EQUAL',
-        [TokenType.EQUAL]: 'EQUAL',
-        [TokenType.EQUAL_EQUAL]: 'EQUAL_EQUAL',
-        [TokenType.GREATER]: 'GREATER',
-        [TokenType.GREATER_EQUAL]: 'GREATER_EQUAL',
-        [TokenType.LESS]: 'LESS',
-        [TokenType.LESS_EQUAL]: 'LESS_EQUAL',
-        [TokenType.IDENTIFIER]: 'IDENTIFIER',
-        [TokenType.STRING]: 'STRING',
-        [TokenType.NUMBER]: 'NUMBER',
-        [TokenType.AND]: 'AND',
-        [TokenType.CLASS]: 'CLASS',
-        [TokenType.ELSE]: 'ELSE',
-        [TokenType.FALSE]: 'FALSE',
-        [TokenType.FUN]: 'FUN',
-        [TokenType.FOR]: 'FOR',
-        [TokenType.IF]: 'IF',
-        [TokenType.NIL]: 'NIL',
-        [TokenType.OR]: 'OR',
-        [TokenType.PRINT]: 'PRINT',
-        [TokenType.RETURN]: 'RETURN',
-        [TokenType.SUPER]: 'SUPER',
-        [TokenType.THIS]: 'THIS',
-        [TokenType.TRUE]: 'TRUE',
-        [TokenType.VAR]: 'VAR',
-        [TokenType.WHILE]: 'WHILE',
-        [TokenType.EOF]: 'EOF'
-    };
-
-    getTokenTypeName(tokenType: TokenType) {
-        const typeName = this.TOKEN_TYPE_NAMES[tokenType];
-        return typeName !== undefined ? typeName : 'Unknown TokeType';
-    }
-
-    constructor(type: TokenType, lexeme: string, literal: any, line: number) {
-        this.type = type;
-        this.lexeme = lexeme;
-        this.literal = literal;
-        this.line = line;
-    }
-
-    public toString(): string {
-        const type = this.getTokenTypeName(this.type);
-        return `${type} ${this.lexeme} ${this.literal ?? ''}`;
-    }
-}
 
 enum TokenType {
     // Single-character tokens.
@@ -112,6 +47,71 @@ enum TokenType {
     VAR,
     WHILE,
     EOF
+}
+
+const TOKEN_TYPE_NAMES = {
+    [TokenType.LEFT_PAREN]: 'LEFT_PAREN',
+    [TokenType.RIGHT_PAREN]: 'RIGHT_PAREN',
+    [TokenType.LEFT_BRACE]: 'LEFT_BRACE',
+    [TokenType.RIGHT_BRACE]: 'RIGHT_BRACE',
+    [TokenType.COMMA]: 'COMMA',
+    [TokenType.DOT]: 'DOT',
+    [TokenType.MINUS]: 'MINUS',
+    [TokenType.PLUS]: 'PLUS',
+    [TokenType.SEMICOLON]: 'SEMICOLON',
+    [TokenType.SLASH]: 'SLASH',
+    [TokenType.STAR]: 'STAR',
+    [TokenType.BANG]: 'BANG',
+    [TokenType.BANG_EQUAL]: 'BANG_EQUAL',
+    [TokenType.EQUAL]: 'EQUAL',
+    [TokenType.EQUAL_EQUAL]: 'EQUAL_EQUAL',
+    [TokenType.GREATER]: 'GREATER',
+    [TokenType.GREATER_EQUAL]: 'GREATER_EQUAL',
+    [TokenType.LESS]: 'LESS',
+    [TokenType.LESS_EQUAL]: 'LESS_EQUAL',
+    [TokenType.IDENTIFIER]: 'IDENTIFIER',
+    [TokenType.STRING]: 'STRING',
+    [TokenType.NUMBER]: 'NUMBER',
+    [TokenType.AND]: 'AND',
+    [TokenType.CLASS]: 'CLASS',
+    [TokenType.ELSE]: 'ELSE',
+    [TokenType.FALSE]: 'FALSE',
+    [TokenType.FUN]: 'FUN',
+    [TokenType.FOR]: 'FOR',
+    [TokenType.IF]: 'IF',
+    [TokenType.NIL]: 'NIL',
+    [TokenType.OR]: 'OR',
+    [TokenType.PRINT]: 'PRINT',
+    [TokenType.RETURN]: 'RETURN',
+    [TokenType.SUPER]: 'SUPER',
+    [TokenType.THIS]: 'THIS',
+    [TokenType.TRUE]: 'TRUE',
+    [TokenType.VAR]: 'VAR',
+    [TokenType.WHILE]: 'WHILE',
+    [TokenType.EOF]: 'EOF'
+};
+class Token {
+    type: TokenType;
+    lexeme: string;
+    literal: any;
+    line: number;
+
+    getTokenTypeName(tokenType: TokenType) {
+        const typeName = TOKEN_TYPE_NAMES[tokenType];
+        return typeName !== undefined ? typeName : 'Unknown TokeType';
+    }
+
+    constructor(type: TokenType, lexeme: string, literal: any, line: number) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    public toString(): string {
+        const type = this.getTokenTypeName(this.type);
+        return `${type} ${this.lexeme} ${this.literal ?? ''}`;
+    }
 }
 
 export { Token, TokenType };
