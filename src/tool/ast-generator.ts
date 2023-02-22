14d1da66bdd5d85cb6fd8107ef2d51fb5a4b5c67
joshbochu <joshuatbochu+git@gmail.com>
2023-02-21 23:16:23 -0800

    complete visitor pattern

--- src/tool/ast-generator.ts
+++ src/tool/ast-generator.ts
@@ -7,6 +7,7 @@ function main(args: string[]): void {
     }
 
     const outputDir = args[0];
+
     defineAst(outputDir, 'Expr', [
         'Binary   : Expr left, Token operator, Expr right',
         'Grouping : Expr expression',
@@ -20,12 +21,33 @@ function defineAst(outputDir: string, baseName: string, types: string[]) {
     const path = `${outputDir}/${baseName.toLowerCase()}.ts`;
     const writer = fs.createWriteStream(path);
 
-    writer.write(`export abstract class ${baseName} {}\n\n`);
+    defineVisitor(writer, baseName, types);
+
+    writer.write(`export abstract class ${baseName} {\n`);
+    writer.write(`\tabstract accept<R>(visitor: ExprVisitor<R>): R;\n}\n\n`);
+
+    // define subclassess that extend abstract class
     for (const t of types) {
         const className = t.split(':')[0].trim();
         const fields = t.split(':')[1].trim();
         defineType(writer, baseName, className, fields);
     }
+}
+
+function defineVisitor(
+    writer: fs.WriteStream,
+    baseName: string,
+    types: string[]
+): void {
+    writer.write(`interface ExprVisitor<R> {\n`);
+
+    for (const type of types) {
+        const typeName = type.split(':')[0].trim();
+        const methodName = `visit${typeName}${baseName}`;
+        writer.write(`\t${methodName}(expr: ${typeName}): R;\n`);
+    }
+
+    writer.write(`}\n\n`);
 }
 
 function defineType(
@@ -46,7 +68,10 @@ function defineType(
         if (i !== fields.length - 1) writer.write(', ');
     }
     writer.write(') {\n');
-    writer.write('super();}}\n\n');
+    writer.write('super();}\n\n');
+    writer.write('\taccept<R>(visitor: ExprVisitor<R>): R {\n');
+    writer.write(`\treturn visitor.visit${className}Expr(this);}\n`);
+    writer.write('}\n\n\n');
 }
 
 main(process.argv.slice(2));
--- src/tool/expr.ts
+++ src/tool/expr.ts
@@ -1,11 +1,23 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Token } from 'src/token';
 
-export abstract class Expr {}
+interface ExprVisitor<R> {
+    visitBinaryExpr(expr: Binary): R;
+    visitGroupingExpr(expr: Grouping): R;
+    visitLiteralExpr(expr: Literal): R;
+    visitUnaryExpr(expr: Unary): R;
+}
+
+export abstract class Expr {
+    abstract accept<R>(visitor: ExprVisitor<R>): R;
+}
 
 export class Binary extends Expr {
     constructor(public left: Expr, public operator: Token, public right: Expr) {
         super();
+    }
+
+    accept<R>(visitor: ExprVisitor<R>): R {
+        return visitor.visitBinaryExpr(this);
     }
 }
 
@@ -13,16 +25,28 @@ export class Grouping extends Expr {
     constructor(public expression: Expr) {
         super();
     }
+
+    accept<R>(visitor: ExprVisitor<R>): R {
+        return visitor.visitGroupingExpr(this);
+    }
 }
 
 export class Literal extends Expr {
     constructor(public value: any) {
         super();
     }
+
+    accept<R>(visitor: ExprVisitor<R>): R {
+        return visitor.visitLiteralExpr(this);
+    }
 }
 
 export class Unary extends Expr {
     constructor(public operator: Token, public right: Expr) {
         super();
+    }
+
+    accept<R>(visitor: ExprVisitor<R>): R {
+        return visitor.visitUnaryExpr(this);
     }
 }
