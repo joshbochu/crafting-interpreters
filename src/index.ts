ead6f6a45900d49231de96a54fac191c31be238e
joshbochu <joshuatbochu+git@gmail.com>
2023-02-22 19:09:17 -0800

    updat lox

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,20 @@
 // import { main } from './lox';
 
-import { AstPrinter } from "./ast-printer";
-import { Token, TokenType } from "./token";
-import { Binary, Grouping, Literal, Unary } from "./tool/expr";
+import { AstPrinter } from './ast-printer';
+import { main } from './lox';
+import { Token, TokenType } from './token';
+import { Binary, Grouping, Literal, Unary } from './tool/expr';
 
-// main();
-
-const main = () => {
-    const expression = new Binary(
-        new Unary(
-          new Token(TokenType.MINUS, "-", null, 1),
-          new Literal(123)
-        ),
-        new Token(TokenType.STAR, "*", null, 1),
-        new Grouping(
-          new Literal(45.67)
-        )
-      );
-  
-      console.log(new AstPrinter().print(expression));
-}
 main();
+
+// const testPrintAst = () => {
+//     const expression = new Binary(
+//         new Unary(new Token(TokenType.MINUS, '-', null, 1), new Literal(123)),
+//         new Token(TokenType.STAR, '*', null, 1),
+//         new Grouping(new Literal(45.67))
+//     );
+
+//     console.log(new AstPrinter().print(expression));
+// };
+
+// testPrintAst();
