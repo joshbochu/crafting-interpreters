85f47b80fa6f522c931dd44043ba728e93ea5806
joshbochu <joshuatbochu+git@gmail.com>
2023-02-26 23:48:55 -0800

    add bangcase

--- src/interpreter.ts
+++ src/interpreter.ts
@@ -1,16 +1,37 @@
-import { Binary, ExprVisitor, Grouping, Literal, Unary } from './expr';
+import { ExprVisitor, Binary, Expr, Grouping, Literal, Unary } from './expr';
+import { TokenType } from './token';
 
-export class Interpreter implements ExprVisitor<object> {
-    visitBinaryExpr(expr: Binary): object {
+export class Interpreter implements ExprVisitor<any> {
+    evaluate(expr: Expr) {
+        return expr.accept(this);
+    }
+
+    visitBinaryExpr(expr: Binary): any {
         throw new Error('Method not implemented.');
     }
-    visitGroupingExpr(expr: Grouping): object {
-        throw new Error('Method not implemented.');
+
+    visitGroupingExpr(expr: Grouping): any {
+        return this.evaluate(expr.expression);
     }
-    visitLiteralExpr(expr: Literal): object {
-        throw new Error('Method not implemented.');
+
+    visitLiteralExpr(expr: Literal): any {
+        return expr.value;
     }
-    visitUnaryExpr(expr: Unary): object {
-        throw new Error('Method not implemented.');
+
+    visitUnaryExpr(expr: Unary): any {
+        const right = this.evaluate(expr.right);
+        switch (expr.operator.type) {
+            case TokenType.MINUS:
+                return -(<number>right);
+            case TokenType.BANG:
+                return !(() => {
+                    if (right === null || right === undefined) return;
+                    if (typeof right === 'boolean') return <boolean>right;
+                    return true;
+                })();
+            default:
+                break;
+        }
+        return null;
     }
 }
