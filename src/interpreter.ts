f2ac1f31a7984591ca116527dfa660d6c7716403
joshbochu <joshuatbochu+git@gmail.com>
2023-02-27 21:48:07 -0800

    add binary stuff

--- src/interpreter.ts
+++ src/interpreter.ts
@@ -7,7 +7,41 @@ export class Interpreter implements ExprVisitor<any> {
     }
 
     visitBinaryExpr(expr: Binary): any {
-        throw new Error('Method not implemented.');
+        const left = this.evaluate(expr.left);
+        const right = this.evaluate(expr.right);
+        switch (expr.operator.type) {
+            case TokenType.GREATER:
+                return <number>left > <number>right;
+            case TokenType.GREATER_EQUAL:
+                return <number>left >= <number>right;
+            case TokenType.LESS:
+                return <number>left < <number>right;
+            case TokenType.LESS_EQUAL:
+                return <number>left <= <number>right;
+            case TokenType.MINUS:
+                return <number>left - <number>right;
+            case TokenType.SLASH:
+                return <number>left / <number>right;
+            case TokenType.STAR:
+                return <number>left * <number>right;
+            case TokenType.BANG_EQUAL:
+                //TODO
+                break;
+            case TokenType.EQUAL_EQUAL:
+                //TODO
+                break;
+            case TokenType.PLUS:
+                if (typeof left === 'number' && typeof right === 'number') {
+                    return <number>left + <number>right;
+                }
+                if (typeof left === 'string' && typeof right === 'string') {
+                    return <string>left + <string>right;
+                }
+                break;
+            default:
+                break;
+        }
+        return null;
     }
 
     visitGroupingExpr(expr: Grouping): any {
