d15ce8788d5c6bb7430765ee5b3b272eaef4da83
joshbochu <joshuatbochu+git@gmail.com>
2023-03-11 22:00:33 -0800

    add-equality

--- src/parser.ts
+++ src/parser.ts
@@ -1,4 +1,5 @@
-import { Token } from './token';
+import { Binary } from './expr';
+import { Token, TokenType } from './token';
 
 class Parser {
     private readonly tokens;
@@ -6,5 +7,15 @@ class Parser {
 
     constructor(tokens: Token[]) {
         this.tokens = tokens;
+    }
+
+    private equality(): Expr {
+        let expr = this.comparison();
+        while (this.match(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL)) {
+            const operator = this.previous();
+            const right = this.comparison();
+            expr = new Binary(expr, operator, right);
+        }
+        return expr;
     }
 }
